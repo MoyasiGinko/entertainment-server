ea1b4ef09ef00b927d7b28311686477b17b32d1a
Moyasi Ginko <mrsiyam61@gmail.com>
2024-02-04 02:41:28 +0600

    remove uid

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,15 +1,19 @@
 // models/Movie.js
-const uuid = require('uuid');
-
 class Movie {
   constructor(title, genre, image, rating, releaseDate, duration) {
-    this.id = uuid.v4();
+    this.id = Movie.generateNextId(); // Assign the next sequential numerical ID
     this.title = title;
     this.genre = genre;
     this.image = image;
     this.rating = rating;
     this.releaseDate = releaseDate;
     this.duration = duration;
+  }
+
+  static nextId = 1; // Initial value for the next ID
+
+  static generateNextId() {
+    return Movie.nextId++;
   }
 }
 
