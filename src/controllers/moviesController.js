004de79192b67c2510c8a3a18d7983971383db51
Moyasi Ginko <mrsiyam61@gmail.com>
2024-02-04 02:10:30 +0600

    Update moviesController.js

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,39 +1,49 @@
 // src/controllers/moviesController.js
+const movies = require('../movieData');
 const Movie = require('../models/Movie');
 
-let movies = [];
+// Initialize movies array with top movies
+let movies = topMovies.map((movie) => new Movie(movie.title, movie.genre, movie.image, movie.rating, movie.releaseDate, movie.duration));
 
-const getAllMovies = (req, res) => {
+// Controller functions
+
+// Get all movies
+function getAllMovies(req, res) {
   res.json(movies);
-};
+}
 
-const createMovie = (req, res) => {
-  const { title, genre } = req.body;
-  const newMovie = new Movie(title, genre);
+// Create a new movie
+function createMovie(req, res) {
+  const { title, genre, image, rating, releaseDate, duration } = req.body;
+  const newMovie = new Movie(title, genre, image, rating, releaseDate, duration);
   movies.push(newMovie);
   res.json(newMovie);
-};
+}
 
-const updateMovie = (req, res) => {
+// Update a movie
+function updateMovie(req, res) {
   const { id } = req.params;
-  const { title, genre } = req.body;
+  const { title, genre, image, rating, releaseDate, duration } = req.body;
   const movie = movies.find((m) => m.id === id);
   if (movie) {
     movie.title = title;
     movie.genre = genre;
+    movie.image = image;
+    movie.rating = rating;
+    movie.releaseDate = releaseDate;
+    movie.duration = duration;
     res.json(movie);
   } else {
     res.status(404).json({ message: 'Movie not found' });
   }
-};
+}
 
-const deleteMovie = (req, res) => {
+// Delete a movie
+function deleteMovie(req, res) {
   const { id } = req.params;
   movies = movies.filter((m) => m.id !== id);
   res.json({ message: 'Movie deleted successfully' });
-};
-
-
+}
 
 module.exports = {
   getAllMovies,
